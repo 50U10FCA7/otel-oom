c46f92a2c67d0beb392aad62b523d0407b05b642
50U10FCA7 <m.bashilov.srg@gmail.com>
2025-03-11 12:18:53 -0500

    Demo [skip ci]

---
+++ src/main.rs
@@ -0,0 +1,58 @@
+use opentelemetry::{global, trace::TracerProvider};
+use opentelemetry_otlp::WithExportConfig;
+use opentelemetry_sdk::{
+    propagation::TraceContextPropagator,
+    runtime::Tokio,
+    trace::{self, SdkTracerProvider, span_processor_with_async_runtime::BatchSpanProcessor},
+};
+use tracing_subscriber::{layer::SubscriberExt as _, util::SubscriberInitExt as _};
+
+#[tokio::main]
+async fn main() {
+    init_tracing();
+
+    loop {
+        do_span()
+    }
+}
+
+#[tracing::instrument(
+    name = "do_span",
+    skip_all,
+    fields(
+        my_long_field = {
+            // 1MB field
+            "a".repeat(1_000_000)
+        }
+    ),
+)]
+fn do_span() {}
+
+fn init_tracing() {
+    global::set_text_map_propagator(TraceContextPropagator::new());
+
+    let exporter = opentelemetry_otlp::SpanExporter::builder()
+        .with_tonic()
+        .with_endpoint("http://localhost:4317".to_string()) // Unreachable host to demonstrate the error
+        .with_protocol(opentelemetry_otlp::Protocol::Grpc)
+        .build()
+        .unwrap_or_else(|e| panic!("failed to initialize `tracing`: {e}"));
+
+    let provider = SdkTracerProvider::builder()
+        .with_span_processor(
+            BatchSpanProcessor::builder(exporter, Tokio)
+                .with_batch_config(
+                    // Limit the batch size to reach OOM faster.
+                    trace::BatchConfigBuilder::default()
+                        .with_max_concurrent_exports(1)
+                        .with_max_export_batch_size(1)
+                        .build(),
+                )
+                .build(),
+        )
+        .build();
+
+    tracing_subscriber::registry()
+        .with(tracing_opentelemetry::layer().with_tracer(provider.tracer("otel")))
+        .init();
+}
